425b7f24ce4f5524e22cf14e98b2540f7535b2d6
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2026-07-27 17:35:12 -0400

    updated config.py with new variable

--- app/config.py
+++ app/config.py
@@ -7,3 +7,4 @@ load_dotenv()
 
 redis_url = os.getenv("REDIS_URL")
 openai_api_key = os.getenv("OPEN_AI_API_KEY")
+index_name = os.getenv("SEARCH_INDEX_NAME")
--- app/services/redis_client.py
+++ app/services/redis_client.py
@@ -1,8 +1,8 @@
 import redis
-from app.config import redis_url
+from app.config import redis_url, index_name
 
 def get_redis_client():
     return redis.Redis(host=redis_url, port=6379, decode_responses=True)
 
 def search(query_embedding):
-    return redis.ft("marine_bio_chunks").search(query_embedding)
+    return redis.ft(index_name).search(query_embedding)
