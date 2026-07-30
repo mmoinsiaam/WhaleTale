98c6dde7546f2a82fc8e7d6a98776a4be0bdbfa9
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2026-07-30 19:05:21 -0400

    added a vpc module

---
+++ terraform/modules/vpc/route_tables.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  # all public traffic to igw
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Component = "networking"
+  }
+}
+
+#explicitly associate public subnet with public route table
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
---
+++ terraform/modules/vpc/subnets.tf
@@ -0,0 +1,20 @@
+resource "aws_subnet" "public" {
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = "10.0.1.0/24"
+    
+    map_public_ip_on_launch = true
+    availability_zone = vars.availability_zone
+    tags = {
+        Component = "networking"
+    }
+}
+
+resource "aws_subnet" "private" {
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = "10.0.2.0/24"
+    map_public_ip_on_launch = false
+    availability_zone = vars.availability_zone
+    tags = {
+        Component = "networking"
+    }
+}
