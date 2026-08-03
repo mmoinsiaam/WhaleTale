8937c9e5f0cf4250c3c3740270157030b4edf25a
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2026-08-02 20:21:26 -0400

    added subnets for availability_zone 2 and attached them to rt

--- terraform/modules/vpc/route_tables.tf
+++ terraform/modules/vpc/route_tables.tf
@@ -18,6 +18,11 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -35,5 +40,10 @@ resource "aws_route_table" "private_rt" {
 #explicitly associate private subnet with private route table
 resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc_2" {
+  subnet_id      = aws_subnet.private_2.id
   route_table_id = aws_route_table.private_rt.id
 }
--- terraform/modules/vpc/vars.tf
+++ terraform/modules/vpc/vars.tf
@@ -6,3 +6,9 @@ variable "availability_zone" {
   type    = string
   default = "us-east-1a"
 }
+
+variable "availability_zone_2" {
+  type    = string
+  default = "us-east-1b"
+}
+
